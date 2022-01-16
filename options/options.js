e6f84825f4d323cad3f95b2dca11b509735bebb5
Crowdbotics <team@crowdbotics.com>
2022-01-16 22:59:58 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "skyhighclub_32887",
+  url: "https://skyhighclub_32887.botics.co",
+  api: "https://skyhighclub_32887.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
